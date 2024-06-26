0609839ece1db479443d89e111b116255a1a8f03
Yuya Isaka <zxwg6329@gmail.com>
2024-06-26 12:55:20 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,17 @@
-#![no_std] // 標準ライブラリ使わないよ
+#![no_std] // 標準ライブラリ(std)使わないよ
 #![no_main] // main関数自分で書くよ
+
+// アセンブリを埋め込むためのマクロをインポート
+//  * coreライブラリはstdライブラリのサブセットで、OSがない環境でも使える
+use core::arch::{asm, global_asm};
+
+// パニック時の処理
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        unsafe {
+            asm!("wfi");
+        }
+    }
+}
